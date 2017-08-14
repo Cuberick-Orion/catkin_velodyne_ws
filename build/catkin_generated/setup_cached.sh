5332ce1eda566f8b5c8c42458f15217adbc23788
Cuberick-Orion <u5689359@anu.edu.au>
2017-08-14 13:14:26 +1000

    try rosbag read

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/cuberick/catkin_velodyne_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/cuberick/catkin_velodyne_ws/build"
