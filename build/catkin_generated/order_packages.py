d4457217113b078f27461cdc436113f4e874a1f0
Cuberick-Orion <u5689359@anu.edu.au>
2017-08-14 10:24:07 +1000

    Initialise

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/cuberick/catkin_velodyne_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/cuberick/catkin_velodyne_ws/devel;/opt/ros/kinetic".split(';') if "/home/cuberick/catkin_velodyne_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ build/velodyne_visual/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cuberick/catkin_velodyne_ws/src/velodyne_visual
+# Build directory: /home/cuberick/catkin_velodyne_ws/build/velodyne_visual
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
